fe43b1f8e4fff2f8bf6ff1b85d5353cd1de4121e
Oleksandra Medvedieva <sashazibt940@gmail.com>
2022-08-01 12:48:15 +0300

    commit all JAVA

--- src/day38_Inheritance/Toyota.java
+++ src/day38_Inheritance/Toyota.java
@@ -3,8 +3,8 @@ package day38_Inheritance;
 import day38_Inheritance.carTask.Car;
 
 public class Toyota extends Car {
-    public Toyota(String brand, String model, String color, int year, double price, double miles) {
-        super(brand, model, color, year, price, miles);
+    public Toyota(String model, String color, int year, double price, double miles) {
+        super("Toyota", model, color, year, price, miles);
     }
     public void reliable(){
         System.out.println(brand + " " + model + " is reliable");
