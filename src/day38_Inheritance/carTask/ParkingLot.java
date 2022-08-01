e6fd163f1e6cddf35c60ceddc68dbd15647820b3
Oleksandra Medvedieva <sashazibt940@gmail.com>
2022-08-01 13:18:59 +0300

    commit

---
+++ src/day38_Inheritance/carTask/ParkingLot.java
@@ -0,0 +1,12 @@
+package day38_Inheritance.carTask;
+
+import day38_Inheritance.Toyota;
+
+public class ParkingLot {
+    public static void main(String[] args) {
+        Toyota toyota = new Toyota("Camry", "red", 2020,20000,2365214);
+        Tesla tesla = new Tesla("Model S", "Grey", 2022,94990,0);
+        BMW bmw = new BMW("X6", "black", 2016,56000,123654);
+
+    }
+}
