3bd315f61683361a3871b8a12c10bbf798cdbfbc
Oleksandra Medvedieva <sashazibt940@gmail.com>
2022-08-02 16:04:32 +0300

    commit

--- src/day39_Recap/shapeTask/Objects.java
+++ src/day39_Recap/shapeTask/Objects.java
@@ -3,14 +3,17 @@ package day39_Recap.shapeTask;
 public class Objects {
     public static void main(String[] args) {
 
-        Circle circle = new Circle(4);
+        Circle circle = new Circle(8);
         Rectangle rectangle = new Rectangle(3,6);
-        Square square = new Square(8);
+        Square square = new Square(10);
 
         System.out.println(circle.area());
         System.out.println(circle.perimeter());
         System.out.println(rectangle.area());
         System.out.println(square.perimeter());
+        System.out.println(circle);
+        System.out.println(square);
+        System.out.println(rectangle);
 
 
     }
