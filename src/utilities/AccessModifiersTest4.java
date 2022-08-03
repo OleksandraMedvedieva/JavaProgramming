fb14971c5a75cb248bf313e0b15aca25acb12a89
Oleksandra Medvedieva <sashazibt940@gmail.com>
2022-08-03 12:14:15 +0300

    commit

--- src/utilities/AccessModifiersTest4.java
+++ src/utilities/AccessModifiersTest4.java
@@ -5,6 +5,7 @@ import day40_FinalKeyword.Protected_AccessModifier;
 public class AccessModifiersTest4 extends Protected_AccessModifier {
     public static void main(String[] args) {
         System.out.println(Protected_AccessModifier.name2);
+        AccessModifiersTest4.method2();
 
     }
 }
